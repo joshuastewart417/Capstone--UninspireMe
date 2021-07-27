e5d4455be36ff290d244294a3b63eb898d608d9e
joshuastewart417 <joshua.stewart780@gmail.com>
2021-07-27 12:18:45 -0500

    initial commit with quote logic

---
+++ src/Data.js
@@ -0,0 +1,35 @@
+export const quoteData =[
+      {
+        "id": 1,
+        "body":"Education without values, as useful as it is, seems rather to make man a more clever devil.",
+        "author": "C.S. Lewis"
+      }  
+    ]
+
+export const nounData = [
+    {
+      "nouns": [
+      "menu",
+      "drama",
+      "tradition",
+      "hall",
+      "grocery",
+      "courage",
+      "satisfaction",
+      "competition",
+      "understanding",
+      "guest",
+      "nation",
+      "refrigerator",
+      "version",
+      "presence",
+      "explanation",
+      "meal",
+      "performance",
+      "outcome"
+    ]
+    }
+]
+    
+
+
---
+++ src/Quotes.js
@@ -0,0 +1,39 @@
+import React, {useState} from "react";
+import nlp from "compromise"
+import {quoteData} from "./Data.js"
+import {nounData} from "./Data.js"
+import "./App.css";
+
+const nounList = (nounData)[0];
+let doc = ""
+
+const filterNoun = (arr) => {
+    const getRandomNoun = Math.floor(Math.random() * arr.length)
+    const randomNoun = arr[getRandomNoun]
+    return randomNoun
+}
+
+const replaceNoun = () => {
+nlp(doc).replaceWith(filterNoun(nounList), {keepTags:true}).text()
+console.log(doc)
+return doc.text()
+}
+
+export const Quote = ({ body, author }) => {
+    doc = nlp(`${body}`).nouns().out('array')
+    console.log(doc)
+  if (!body) return <div />;
+  return (
+        <div>
+            {quoteData.find((body, author))}
+          <div>
+            <h5>{body}</h5>
+          </div>
+          <div>
+            <h5>{author}</h5>
+          </div>
+          <button onClick={replaceNoun}>Uninspire Me</button>
+        </div>
+  );
+};
+
